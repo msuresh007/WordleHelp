001e72a2082c69a08f1193cf4f54b4126ae341d7
Suresh Madadha <42271127+msuresh007@users.noreply.github.com>
2022-03-09 22:24:34 +0530

    fix for not looking for all -with characters

--- WordleHelp.ps1
+++ WordleHelp.ps1
@@ -5,9 +5,23 @@ param(
     [String]$exactPosition = ""
 )
 
+function containsAllChars([string] $inputStr, [string] $withChars)
+{
+    $allCharsArePresent = $true
+
+    foreach ($char in $withChars.ToCharArray())   {
+        if ($inputStr -notmatch '['+$char+']')  {
+            $allCharsArePresent = $false
+            break
+        }
+    }
+
+    return $allCharsArePresent
+}
+
 $wordsList = Import-Csv -Path .\words5.csv 
 
-$wordsList = $wordsList | Where-Object { $with -eq "" -or $_.word -match '[' + $with + ']'}
+$wordsList = $wordsList | Where-Object { $with -eq "" -or (containsAllChars $_.word $with) -eq $true }  
                         | Where-Object { $withOut -eq "" -or $_.word -notmatch '[' + $withOut + ']'}  
                         | Where-Object { $exactPosition -eq "" -or $_.word -match $exactPosition.Replace('_', '[a-z]') }  
 
